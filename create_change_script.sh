4d9ea26065989e1cf0daed31026e06b483219c5e
jacquelineflemming <jacquelinehfl@gmail.com>
2018-10-22 11:39:22 -0500

    Update create_change_script.sh

--- create_change_script.sh
+++ create_change_script.sh
@@ -1,4 +1,8 @@
 # use changepass.xp
+#
+# Can also be run as this: ~/scripts_new/changepass2.xp jflemming "OLD PASSWORD 222" e32 "NEW PASSWORD 05"
+# 
+#
 for i in $(cat host_list_proxy_201704.txt)
 do
 
